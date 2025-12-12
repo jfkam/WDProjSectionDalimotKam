b316b8a0660a64922cdfe76156f5c8ac09656cc1
ellysz24 <b2029ketdalimot@pshs.edu.ph>
2025-12-12 20:56:19 +0800

    fix pages v2

--- public/Home.js
+++
@@ -1,7 +0,0 @@
- document.addEventListener("DOMContentLoaded", () => {
-            const usernameSpan = document.getElementById("username");
-            if (usernameSpan) {
-                const user = localStorage.getItem("currentUser") || "User";
-                usernameSpan.textContent = user;
-            }
-        });
--- public/sources.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-            const user = localStorage.getItem("currentUser") || "User";
-            document.getElementById("username").innerText = user;
-        });
